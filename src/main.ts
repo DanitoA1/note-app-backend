47a7e69d8edcbc2863a92f9c7c2e3a48def34ac5
DanitoA1 <ahiabadaniel@gmail.com>
2024-08-31 15:31:31 +0100

    setup swagger for proper documentation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import * as fs from 'fs';
+
+const logger = new Logger('bootstrap');
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
-  app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.enableCors();
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Note App API')
+    .setDescription('The Note App API documentation')
+    .setVersion('1.0')
+    .addTag('notes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
-  await app.listen(4000);
+  fs.writeFileSync('swagger-spec.json', JSON.stringify(document));
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(PORT, async () => {
+    logger.log(`Application is running on: ${await app.getUrl()}`);
+  });
 }
 bootstrap();
